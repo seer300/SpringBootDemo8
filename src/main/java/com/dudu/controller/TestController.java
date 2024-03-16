e665932014ce8d0fdd5f67af20a7e4df65359403
seer300 <1824855220@qq.com>
2024-03-16 20:08:42 +0800

    调通SQL数据库，HTTP接口OK;

--- src/main/java/com/dudu/controller/TestController.java
+++ src/main/java/com/dudu/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.dudu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,21 @@ import java.util.Map;
 @RequestMapping("/test")
 public class TestController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @RequestMapping("/test1")
     public Map<String,Object> test1(){
         Map<String,Object> map = new HashMap<>();
         map.put("value", "1");
         return map;
+    }
+
+    @RequestMapping("/test2")
+    public Object test2() {
+        String sql = "SELECT count(*) FROM `JYT_dbs`.`CaseReview`";
+        int i = this.jdbcTemplate.queryForObject(sql, Integer.class);
+        return i;
     }
 
 }
