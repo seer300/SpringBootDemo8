582d1fb4bc870744c3cacb6fb3a43902fe7a2729
seer300 <1824855220@qq.com>
2024-03-19 22:18:05 +0800

    新数据库适配;

--- src/main/java/com/dudu/controller/FaultController.java
+++ src/main/java/com/dudu/controller/FaultController.java
@@ -18,7 +18,7 @@ public class FaultController {
     //获取热点案例
     @RequestMapping("/hot")
     public List<Map<String, Object>> getHotFaults(){
-        String sql = "SELECT * FROM `JYT_dbs`.`FaultMain` ORDER BY RAND() LIMIT 5";
+        String sql = "SELECT * FROM `JYT_dbs`.`JYT_Claim_Main` ORDER BY RAND() LIMIT 5";
         List<Map<String, Object>> data = jdbcTemplate.queryForList(sql);
         return data;
     }
@@ -26,17 +26,22 @@ public class FaultController {
     //获取所有案例数据
     @RequestMapping("/all")
     public List<Map<String, Object>> getAllFaults(){
-        String sql = "SELECT * FROM `JYT_dbs`.`FaultMain` LIMIT 100";
+        String sql = "SELECT * FROM `JYT_dbs`.`JYT_Claim_Main` LIMIT 100";
         List<Map<String, Object>> data = jdbcTemplate.queryForList(sql);
         return data;
     }
 
     //获取个人收藏案例
-    @RequestMapping("/star")
+    @RequestMapping("/starlist")
     public List<Map<String, Object>> getStarFaults(){
-        String sql = "SELECT * FROM `JYT_dbs`.`FaultMain` LIMIT 100";
-//        List<Map<String, Object>> data = jdbcTemplate.queryForList(sql);
-        return null;
+        String uid = "User1";
+        String sql = "SELECT JYT_Claim_Main.*, JYT_Case_collect.F_PKId FROM JYT_Case_collect INNER JOIN JYT_Claim_Main ON JYT_Case_collect.F_PKId = JYT_Claim_Main.F_PKId WHERE JYT_Case_collect.UserID = ?";
+        return jdbcTemplate.queryForList(sql,uid);
     }
+
+    //添加个人收藏案例
+
+    //移除个人收藏案例
+
 
 }
